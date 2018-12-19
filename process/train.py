98678cd52bf65215285f538e2db14c5a4a9df06d
wangchenyao <wangchenyao@imdada.cn>
2018-12-19 20:59:41 +0800

    Finish

--- process/train.py
+++ process/train.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     (emb_matrix, word2index, index2word) = pl.load(open(config.embedding_pickle_path, "rb"))
 
     print("载入训练与测试数据...")
-    train_size = test_size = 100000
+    train_size = test_size = 51000
     train_data = pd.read_csv(config.train_path, sep='\t')[:train_size]
     test_data = pd.read_csv(config.test_path, sep='\t')[:test_size]
 
