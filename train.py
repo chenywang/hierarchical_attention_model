4c9e6e2c52c153fc169de4dfb61a9178e3c56dbd
wangchenyao <wangchenyao@imdada.cn>
2018-12-28 17:25:02 +0800

    remove CUDA VISIBLE

--- train.py
+++ train.py
@@ -13,7 +13,6 @@ from attention_model.hierarchical_attention_model import HierarchicalModel
 from config import log_path, train_path, test_path, embedding_pickle_path
 from util.data_utils import gen_batch_train_data
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parameters for building the model.')
